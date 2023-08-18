1a448c0398699231c4570efa28f51edb05e21125
youssefsoua <youssefsoua94@gmail.com>
2023-08-18 21:30:43 +0100

    code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,74 @@
+interface IVehicle {
+  type: string;
+  imageSrc: string;
+  heading: string;
+  description: string;
+  bgColor: string;
+  btnTextColor: string;
+}
+
+const vehicleData: IVehicle[] = [
+  {
+    type: "sedans",
+    imageSrc: "./icon-sedans.svg",
+    heading: "SEDANS",
+    description:
+      "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    bgColor: "bg-bright-orange",
+    btnTextColor: "text-bright-orange",
+  },
+  {
+    type: "suvs",
+    imageSrc: "./icon-suvs.svg",
+    heading: "SUVS",
+    description:
+      "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    bgColor: "bg-dark-cyan",
+    btnTextColor: "text-dark-cyan",
+  },
+  {
+    type: "luxury",
+    imageSrc: "./icon-luxury.svg",
+    heading: "LUXURY",
+    description:
+      "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    bgColor: "bg-very-dark-cyan",
+    btnTextColor: "text-very-dark-cyan",
+  },
+];
+
+function VehicleBlock(props: IVehicle) {
+  const { imageSrc, heading, description, bgColor, btnTextColor } = props;
+  return (
+    <section
+      className={`flex flex-col p-12 ${bgColor} lg:h-[500px] lg:w-[308px] lg:p-8`}
+    >
+      <header>
+        <img className="mb-10 w-16" src={imageSrc} alt={heading} />
+        <h1 className="mb-10 font-big-shoulders text-4xl text-very-light-gray">
+          {heading}
+        </h1>
+      </header>
+
+      <p className="mb-6 font-Lexend-Deca text-transparent-white lg:mb-24 lg:max-w-[210px]">
+        {description}
+      </p>
+      <button
+        className={`w-fit rounded-3xl border bg-white px-8 py-3 font-Lexend-Deca ${btnTextColor} hover:border-white hover:${bgColor} hover:text-white`}
+      >
+        Learn More
+      </button>
+    </section>
+  );
+}
+
 function App() {
   return (
-    <div className=" my-10 flex w-[90%] flex-col overflow-hidden rounded-xl xl:my-0 xl:w-2/3 xl:flex-row">
-      <div className=" flex flex-col bg-bright-orange p-14">
-        <div className=" flex flex-col">
-          <img className="mb-10 w-16" src="./icon-sedans.svg" alt="sedan" />
-          <h1 className="mb-8 font-big-shoulders text-4xl text-very-light-gray">
-            SEDANS
-          </h1>
-          <p className="mb-6 font-Lexend-Deca text-transparent-white xl:mb-20">
-            Choose a sedan for its affordability and excellent fuel economy.
-            Ideal for cruising in the city or on your next road trip.
-          </p>
-          <a className="w-fit rounded-3xl border bg-white px-8 py-3 font-Lexend-Deca text-bright-orange hover:border-white hover:bg-bright-orange hover:text-white">
-            Learn More
-          </a>
-        </div>
-      </div>
-      <div className=" flex flex-col bg-dark-cyan p-14">
-        <div className=" flex flex-col">
-          <img className="mb-10 w-16" src="./icon-suvs.svg" alt="SUVS" />
-          <h1 className="mb-8 font-big-shoulders text-4xl text-very-light-gray">
-            SUVS
-          </h1>
-          <p className="mb-6 font-Lexend-Deca text-transparent-white xl:mb-20">
-            Take an SUV for its spacious interior, power, and versatility.
-            Perfect for your next family vacation and off-road adventures.
-          </p>
-          <a className="w-fit rounded-3xl border bg-white px-8 py-3 font-Lexend-Deca text-dark-cyan  hover:border-white hover:bg-dark-cyan hover:text-white">
-            Learn More
-          </a>
-        </div>
-      </div>
-      <div className=" flex flex-col bg-very-dark-cyan p-14">
-        <div className=" flex flex-col">
-          <img className="mb-10 w-16" src="./icon-luxury.svg" alt="LUXURY" />
-          <h1 className="mb-8 font-big-shoulders text-4xl text-very-light-gray">
-            LUXURY
-          </h1>
-          <p className="mb-6 font-Lexend-Deca text-transparent-white xl:mb-20">
-            Cruise in the best car brands without the bloated prices. Enjoy the
-            enhanced comfort of a luxury rental and arrive in style.
-          </p>
-          <a className="w-fit rounded-3xl border bg-white px-8 py-3 font-Lexend-Deca text-very-dark-cyan hover:border-white hover:bg-very-dark-cyan hover:text-white">
-            Learn More
-          </a>
-        </div>
-      </div>
-    </div>
+    <article className="my-10 flex w-[88%] flex-col items-center overflow-hidden rounded-xl lg:my-0 lg:w-fit lg:flex-row">
+      {vehicleData.map((vehicle) => (
+        <VehicleBlock key={vehicle.type} {...vehicle} />
+      ))}
+    </article>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,12 @@ export default {
         "big-shoulders": ["Big Shoulders Display", "cursive"],
         "Lexend-Deca": ["Lexend Deca", "sans-serif"],
       },
-      screens: {
-        mobile: "480px",
-        "2k": "1440px",
-      },
       colors: {
         "transparent-white": "hsla(0, 0%, 100%, 0.75)",
         "very-light-gray": "hsl(0, 0%, 95%)",
         "bright-orange": "hsl(31, 77%, 52%)",
         "dark-cyan": "hsl(184, 100%, 22%)",
         "very-dark-cyan": "hsl(179, 100%, 13%)",
-      },
-      minHeight: {
-        "1/4": "25%",
       },
     },
   },
